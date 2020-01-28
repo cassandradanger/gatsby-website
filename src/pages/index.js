25eb104322938cf461af40b33eb4f9ae6ec7ce16
Casie Siekman <casie.siekman@gmail.com>
2020-01-28 13:54:54 -0600

    update style

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,8 +164,8 @@ class Homepage extends React.Component {
           <Content>
             <t.H2 primary align="center" bold className="portfolio">
               Experience
+              <img class="center" src="https://boardgamegeek.com/jswidget.php?username=CassandraDanger&numitems=5&header=1&text=title&images=small&show=recentplays&imagesonly=1&imagepos=right&inline=1&addstyles=1&showplaydate=1&domains%5B%5D=boardgame&imagewidget=1" border="0"/>
             </t.H2>
-            <img src="https://boardgamegeek.com/jswidget.php?username=CassandraDanger&numitems=5&header=1&text=title&images=small&show=recentplays&imagesonly=1&imagepos=right&inline=1&addstyles=1&showplaydate=1&domains%5B%5D=boardgame&imagewidget=1" border="0"/>        
           </Content>
           <Block>
             <BlockContent>
